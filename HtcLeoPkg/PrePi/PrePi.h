9369131962b8257d053c9e930348709ee5f325c4
Dominduchami <dominikkobinski314@gmail.com>
2023-09-26 08:57:03 +0200

    PrePi: Reconfigure framebuffer to 32bpp ARGB8888

--- HtcLeoPkg/PrePi/PrePi.h
+++ HtcLeoPkg/PrePi/PrePi.h
@@ -44,6 +44,41 @@ extern UINT64  mSystemMemoryEnd;
 #define GreenYellow     0xAFE5      /* 173, 255,  47 */
 #define Pink            0xF81F
 
+/* MDP-related defines */
+#define MSM_MDP_BASE1 	0xAA200000
+#define LCDC_BASE     	0xE0000
+
+#define BIT(x)  (1<<(x))
+#define DMA_DSTC0G_8BITS (BIT(1)|BIT(0))
+#define DMA_DSTC1B_8BITS (BIT(3)|BIT(2))
+#define DMA_DSTC2R_8BITS (BIT(5)|BIT(4))
+#define CLR_G 0x0
+#define CLR_B 0x1
+#define CLR_R 0x2
+#define MDP_GET_PACK_PATTERN(a,x,y,z,bit) (((a)<<(bit*3))|((x)<<(bit*2))|((y)<<bit)|(z))
+#define DMA_PACK_TIGHT                      BIT(6)
+#define DMA_PACK_LOOSE                      0
+#define DMA_PACK_ALIGN_LSB                  0
+#define DMA_PACK_PATTERN_RGB				\
+        (MDP_GET_PACK_PATTERN(0,CLR_R,CLR_G,CLR_B,2)<<8)
+#define DMA_PACK_PATTERN_BGR \
+       (MDP_GET_PACK_PATTERN(0, CLR_B, CLR_G, CLR_R, 2)<<8)
+#define DMA_DITHER_EN                       BIT(24)
+#define DMA_OUT_SEL_LCDC                    BIT(20)
+#define DMA_IBUF_FORMAT_RGB565              BIT(25)
+#define DMA_IBUF_FORMAT_RGB888              (0 << 25)
+#define DMA_IBUF_FORMAT_xRGB8888_OR_ARGB8888  BIT(26)
+
+/* MDP regs */
+#define REG_MDP(offset)                       MSM_MDP_BASE1 + offset
+
+#define MDP_DMA_P_CONFIG                      REG_MDP(0x90000)
+#define MDP_DMA_P_OUT_XY                      REG_MDP(0x90010)
+#define MDP_DMA_P_SIZE                        REG_MDP(0x90004)
+#define MDP_DMA_P_BUF_ADDR                    REG_MDP(0x90008)
+#define MDP_DMA_P_BUF_Y_STRIDE                REG_MDP(0x9000C)
+#define MDP_DMA_P_OP_MODE                     REG_MDP(0x90070)
+
 RETURN_STATUS
 EFIAPI
 TimerConstructor (
